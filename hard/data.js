5d6e534ac0f53cc9b31575e421ea7705c5c4f8dd
joao.junior <juniorgtr123@gmail.com>
2020-06-13 20:00:01 -0300

    atualização de CSS e JS com novos icones e cores.

---
+++ hard/data.js
@@ -0,0 +1,20 @@
+export default [{
+        title: "Hotline Bling",
+        artist: "Drake",
+        cover: "drake.jpg",
+        file: "Drake - Hotline Bling.mp3"
+    },
+    {
+        title: "Bruno Mars - Uptown Funk ",
+        artist: "Bruno Mars",
+        cover: "bruno.jpg",
+        file: "Mark Ronson - Uptown Funk (Official Video) ft. Bruno Mars.mp3"
+    },
+    {
+        title: "Don't Start Now",
+        artist: "Dua Lipa",
+        cover: "dualipa.jpg",
+        file: "Dua Lipa - Don't Start Now (Official Music Video).mp3"
+    }
+
+];
